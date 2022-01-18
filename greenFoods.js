97c03eeac349ba04b442f3bf6d3508245ea4f503
iuriasydes <iuriasydes@gmail.com>
2022-01-18 11:04:20 -0800

    Subiendo archivos y assets

---
+++ greenFoods.js
@@ -0,0 +1,31 @@
+let slideshow = document.getElementById("slideshow");
+let slides = slideshow.getElementsByTagName("img");
+let index = 0;
+function nextSlide() {
+  slides[index].classList.remove("active");
+  index = (index + 1) % slides.length;
+  slides[index].classList.add("active");
+}
+function prevSlide() {
+  slides[index].classList.remove("active");
+  index = (index - 1 + slides.length) % slides.length;
+  slides[index].classList.add("active");
+}
+
+let slideshowText = document.getElementById("slideshowText");
+let slidesText = slideshowText.getElementsByTagName("div");
+let i = 0;
+function nextSlideText() {
+  slidesText[i].classList.remove("active");
+  i = (i + 1) % slidesText.length;
+  slidesText[i].classList.add("active");
+}
+function prevSlideText() {
+  slidesText[i].classList.remove("active");
+  i = (i - 1 + slidesText.length) % slidesText.length;
+  slidesText[i].classList.add("active");
+}
+function menuToggle() {
+  let nav = document.getElementById("navbar");
+  nav.classList.toggle("active");
+}
